54652c673e5f8231a0617ab248d8aa06566b4cc1
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-03-14 08:35:56 +0100

    First commit

---
+++ src/examen/Ortega.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen;
+
+/**
+ *
+ * @author dam
+ */
+public class Ortega {
+    String primerApellido;
+    int oneplusone=7;
+}
