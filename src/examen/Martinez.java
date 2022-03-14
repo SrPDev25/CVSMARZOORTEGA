8de493268283432d58e61f7ccf323cfae9320db3
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-03-14 08:40:54 +0100

    Second commit

---
+++ src/examen/Martinez.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen;
+
+/**
+ *
+ * @author dam
+ */
+public class Martinez {
+    int onePlusOne=2;
+    boolean truers=false;
+}
